1f299797c9d81b44b2da92c4631a9f782a60c36f
irinastojsic <43534301+irinastojsic@users.noreply.github.com>
2018-10-23 22:27:02 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,37 @@
+var myImage;
+var mySong;
+var w;
+
 function preload(){
-  // put preload code here
+  myImage = loadImage ('./assets/elephant.gif');
+
+  mySong = loadSound('./assets/elephant never forgets.mp3');
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas (windowWidth,windowHeight);
+  mySong.play();
+  fft = new p5.FFT (0.7,512);
+  w = width / 150;
+
 }
 
 function draw() {
-  // put drawing code here
+background(0);
+
+image (myImage, 600, 180, myImage.width+50, myImage.height+50);
+
+var spectrum = fft.analyze();
+noStroke();
+
+for (var i = 0; i < 250; i++){
+  var amp = spectrum[i];
+  var y = map(amp,0,width,height,0);
+  fill(i,255,255);
+
+  rect(i*w, y, w - 2, height - y);
+  rect(i*w, height - y, w - 2, y - height);
+}
+
 }
